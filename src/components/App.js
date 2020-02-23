1e4a7fce89d763b8959bd81b64b4efab3c60cc00
Nagaraj <nagaraj@Nagarajs-iMac-Pro.local>
2020-02-23 16:20:22 +0530

    test-commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
+      <img style={{ height: "60vh" }} src="/logo.png" alt="Logo" />
     </div>
   );
 }
